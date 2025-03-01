ecf75900e7d4b7a7656e464539aa0c25dec70981
e165747 <e165747@ie.u-ryukyu.ac.jp>
2025-03-02 00:06:56 +0900

    chapter-3 practice

---
+++ sql/chapter_3/3-2.sql
@@ -0,0 +1,6 @@
+SELECT actor_id,
+  first_name,
+  last_name
+FROM actor
+WHERE last_name = 'WILLIAMS'
+  OR last_name = 'DAVIS'
---
+++ sql/chapter_3/3-3.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT rental_id
+FROM rental
+WHERE DATE(rental_date) = '2005-07-05';
